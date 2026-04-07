bcd070bfbf07cba1ce4c430bc274101482560431
MERLY LEONELA RIVERA ELIZALDE <mlriverae@utn.edu.ec>
2026-04-07 16:24:58 -0500

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,29 +1,33 @@
 import pandas as pd
 import os
-import time
 
 def guardar_en_historial(archivo_hist, nombre_dataset, total, ataques, tiempo, fecha_simulada, puerto_top):
-    # Creamos el registro con la fecha que elegiste en el calendario
+    # Creamos el registro con la fecha elegida
     nuevo_registro = pd.DataFrame([{
-        "Fecha": fecha_simulada,
+        "Fecha": str(fecha_simulada),
         "Dataset": nombre_dataset,
-        "Registros_Procesados": total,
-        "Ataques_Detectados": ataques,
+        "Registros": total,
+        "Ataques": ataques,
         "Puerto_Critico": puerto_top,
-        "Tiempo_Ejecucion_Seg": round(tiempo, 2)
+        "Tiempo_Seg": round(tiempo, 2)
     }])
     
-    # Si el archivo no existe, lo crea con cabecera. Si existe, añade fila abajo.
-    if not os.path.isfile(archivo_hist):
+    # Guardado seguro
+    if not os.path.isfile(archivo_hist) or os.path.getsize(archivo_hist) == 0:
         nuevo_registro.to_csv(archivo_hist, index=False)
     else:
         nuevo_registro.to_csv(archivo_hist, mode='a', header=False, index=False)
 
 def obtener_puerto_critico(df, predicciones):
-    # Filtramos solo las filas que la IA marcó como ataque (1)
+    # Filtramos solo las filas detectadas como ataque
     ataques_df = df[predicciones == 1]
+    
     if not ataques_df.empty:
-        # Buscamos el puerto que más se repite en la columna ' Destination Port'
-        # El [0] es porque mode() devuelve una lista
-        return ataques_df[' Destination Port'].mode()[0]
+        # Buscamos dinámicamente la columna que contenga 'Port'
+        cols_puerto = [c for c in df.columns if 'Port' in c]
+        if cols_puerto:
+            nombre_col = cols_puerto[0]
+            # Retorna el valor más frecuente (Moda)
+            return ataques_df[nombre_col].mode()[0]
+            
     return "N/A"
