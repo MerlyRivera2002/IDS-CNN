b93bee1e34360f4b9d49d6ec7b580521898077d2
MERLY LEONELA RIVERA ELIZALDE <mlriverae@utn.edu.ec>
2026-04-08 00:23:58 -0500

    Update logic.py

--- logic.py
+++ logic.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 
 def guardar_en_historial(archivo_hist, nombre_dataset, total, ataques, tiempo, fecha_simulada, puerto_top, acc):
-    """Guarda los resultados de la simulación. Recibe exactamente 8 argumentos."""
+    """Guarda los resultados de la simulación con métricas de rendimiento."""
     nuevo_registro = pd.DataFrame([{
         "Fecha": str(fecha_simulada),
         "Dataset": nombre_dataset,
@@ -20,7 +20,6 @@ def guardar_en_historial(archivo_hist, nombre_dataset, total, ataques, tiempo, f
         nuevo_registro.to_csv(archivo_hist, mode='a', header=False, index=False)
 
 def obtener_metricas_resumen(archivo_hist):
-    """Carga los datos para las gráficas de la Pestaña 2."""
     if os.path.exists(archivo_hist):
         try:
             df = pd.read_csv(archivo_hist)
